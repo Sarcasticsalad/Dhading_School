821cfd44f947e08b4c8ebfbe14a77f9bba23d18a
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-06 20:24:20 +0545

    Added the cards for all schools and made it responsive for all screen sizes

--- resources/views/layouts/schools.blade.php
+++ resources/views/layouts/schools.blade.php
@@ -1,5 +1,45 @@
-<div class="flex justify-center">
+<div class="flex flex-col gap-14 sm:gap-16 lg:gap-20">
+  
+    <div class="flex justify-center">
+        <h1 class="text-DDS-blue font-semibold text-3xl sm:text-4xl md:text-4xl lg:text-4xl xl:text-5xl">Our Schools</h1>
+    </div>
 
-<h1 class="text-DDS-blue font-semibold">Our Schools</h1>
+    <div class="flex flex-col gap-24 sm:gap-28 lg:gap-32 xl:flex-row xl:gap-16 2xl:gap-28">
+        <div class="cursor-pointer">
+            <div class="relative h-[19rem] bg-DDS-blue rounded-xl w-[14rem] sm:h-[22rem] sm:w-[20rem] lg:h-[26rem] lg:w-[24rem] xl:w-[21rem] xl:h-[26rem] 2xl:w-[22rem] 2xl:h-[28rem]">
+                <div class="absolute bottom-1.5 right-1.5">
+                    <img src="{{ asset('pictures/Primary School.jpg') }}" class="rounded-t-xl h-[14rem] w-full sm:h-[17rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[22.5rem] object-cover">
+                    <div class="bg-gray-200 flex justify-center rounded-b-xl">
+                        <h1 class="py-7 text-lg sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-semibold">Elementary School</h1>
+                    </div>
+                </div>
+            </div> 
+        </div>
+
+        <div class="cursor-pointer">
+            <div class="relative h-[19rem]  bg-DDS-blue rounded-xl w-[14rem] sm:h-[22rem] sm:w-[20rem] lg:h-[26rem] lg:w-[24rem] xl:w-[21rem] xl:h-[26rem] 2xl:w-[22rem] 2xl:h-[28rem]">
+                <div class="absolute bottom-1.5 right-1.5">
+                    <img src="{{ asset('pictures/Middle School.jpg') }}" class="rounded-t-xl h-[14rem] w-full sm:h-[17rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[22.5rem] object-cover">
+                    <div class="bg-gray-200 flex justify-center rounded-b-xl">
+                        <h1 class="py-7 text-lg sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-semibold">Middle School</h1>
+                    </div>
+                </div>
+            </div> 
+        </div>
+
+        <div class="cursor-pointer">
+            <div class="relative h-[19rem]  bg-DDS-blue rounded-xl w-[14rem] sm:h-[22rem] sm:w-[20rem] lg:h-[26rem] lg:w-[24rem] xl:w-[21rem] xl:h-[26rem] 2xl:w-[22rem] 2xl:h-[28rem]">
+                <div class="absolute bottom-1.5 right-1.5">
+                    <img src="{{ asset('pictures/High School.jpg') }}" class="rounded-t-xl h-[14rem] w-full sm:h-[17rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[22.5rem] object-cover">
+                    <div class="bg-gray-200 flex justify-center rounded-b-xl">
+                        <h1 class="py-7 text-lg sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-semibold">High School</h1>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
 
 </div>
+
+
+
