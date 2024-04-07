159dc3549a5c3632f693b6f7efa195cc6a282732
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-07 15:46:18 +0545

    Added the admission procedure section to the landing page

---
+++ resources/views/layouts/admission_procedure.blade.php
@@ -0,0 +1,85 @@
+<div class="mt-12 flex flex-col gap-4 justify-center sm:mt-14 xl:mt-20">
+
+    <h1 class="text-DDS-blue text-3xl font-semibold py-5 text-center md:text-4xl xl:text-5xl">Admission Procedure</h1>
+
+    <div class="flex flex-col place-items-center sm:mt-1 lg:hidden">
+
+        <div class="flex flex-col gap-8">
+
+            <div class="flex flex-row items-center gap-5 ">
+                <h1 class="text-DDS-blue text-5xl font-bold">01</h1>
+                <p class="text-lg sm:text-xl sm:font-semibold">Engage in Open House Sessions or Counselling</p>
+            </div>
+    
+            <div class="flex flex-row items-center gap-5 ">
+                <h1 class="text-DDS-blue text-5xl font-bold">02</h1>
+                <p class="text-lg sm:text-xl sm:font-semibold">Submit the application form</p>
+            </div>
+    
+            <div class="flex flex-row items-center gap-5 ">
+                <h1 class="text-DDS-blue text-5xl font-bold">03</h1>
+                <p class="text-lg sm:text-xl sm:font-semibold">Take the Deerwalk Aptitude Test (DAT)</p>
+            </div>
+    
+            <div class="flex flex-row items-center gap-5 ">
+                <h1 class="text-DDS-blue text-5xl font-bold">04</h1>
+                <p class="text-lg sm:text-xl sm:font-semibold">Participate in Parent-Teacher Interaction</p>
+            </div>
+    
+            <div class="flex flex-row items-center gap-5 ">
+                <h1 class="text-DDS-blue text-5xl font-bold">05</h1>
+                <p class="text-lg sm:text-xl sm:font-semibold">Receive Admission Open Letter</p>
+            </div>
+    
+            <div class="flex flex-row items-center gap-5 ">
+                <h1 class="text-DDS-blue text-5xl font-bold">06</h1>
+                <p class="text-lg sm:text-xl sm:font-semibold">Confirm Admission</p>
+            </div>
+
+        </div>
+        
+        
+    </div>
+
+    <div class="hidden lg:flex lg:flex-row lg:justify-between lg:mx-16 2xl:mx-64 lg:mt-8">
+
+        <div class="flex flex-col gap-14 mx-10">
+
+            <div class="flex flex-row gap-5 items-center">
+                <h1 class="text-DDS-blue text-7xl font-bold">01</h1>
+                <p class="text-xl">Engage in Open House Sessions or Counselling</p>
+            </div>
+    
+            <div class="flex flex-row gap-5 items-center">
+                <h1 class="text-DDS-blue text-7xl font-bold">03</h1>
+                <p class="text-xl">Take the Deerwalk Aptitude Test (DAT)</p>
+            </div>
+    
+            <div class="flex flex-row gap-5 items-center">
+                <h1 class="text-DDS-blue text-7xl font-bold">05</h1>
+                <p class="text-xl">Receive Admission Open Letter</p>
+            </div>
+        </div>
+
+        <div class="flex flex-col gap-14 mx-10">
+
+            <div class="flex flex-row gap-5 items-center">
+                <h1 class="text-DDS-blue text-7xl font-bold">02</h1>
+                <p class="text-xl">Submit the application form</p>
+            </div>
+
+            <div class="flex flex-row gap-5 items-center">
+                <h1 class="text-DDS-blue text-7xl font-bold">04</h1>
+                <p class="text-xl">Participate in Parent-Teacher Interaction</p>
+            </div>
+    
+            <div class="flex flex-row gap-5 items-center">
+                <h1 class="text-DDS-blue text-7xl font-bold">06</h1>
+                <p class="text-xl">Confirm Admission</p>
+            </div>
+        </div>
+
+    </div>
+
+    
+</div>
