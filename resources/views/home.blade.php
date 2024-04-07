0c5f657d786644773dff5cb695cf2c5f1a24abd0
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-07 15:49:35 +0545

    Minor updates to the text size, spacing and alignment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,6 +24,10 @@
     @include('layouts.chooseDSS')
    </div>
 
+   <div>
+    @include('layouts.admission_procedure')
+   </div>
+
    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>   
    <script src="{{ mix('/resources/js/hamburger.js') }}"></script>
