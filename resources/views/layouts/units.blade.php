a5764eea9fb1ea6cd78c34f4868aa4874c0ddaa6
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-07 17:32:54 +0545

    Added Courses section to landing page

---
+++ resources/views/layouts/units.blade.php
@@ -0,0 +1,51 @@
+<div class="flex flex-col mt-28">
+
+    <div class="flex justify-center">
+        <h1 class="text-DDS-blue text-3xl font-semibold md:text-4xl lg:5xl 2xl:text-5xl">Our Units</h1>
+    </div>
+    
+    <div class="flex flex-col mt-12 gap-14 items-center lg:flex-row lg:justify-evenly px-14">
+
+        <div class="cursor-pointer">
+            <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute w-full bottom-24 sm:bottom-28 lg:bottom-16 xl:bottom-24 2xl:bottom-32">
+                        <img src="{{ asset('') }}">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="cursor-pointer">
+            <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-16 xl:bottom-24 2xl:bottom-32">
+                        <img src="{{ asset('') }}">
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <div class="cursor-pointer">
+            <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-20 xl:bottom-32">
+                        <img src="{{ asset('') }}">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="cursor-pointer">
+            <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-20 xl:bottom-32">
+                        <img src="{{ asset('') }}">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    
+    </div>
