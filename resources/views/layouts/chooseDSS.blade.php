f28488a7d410232df5c911f6017312a31b8b7547
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-07 10:57:17 +0545

    Added the Why Choose DDS section to the landing page

---
+++ resources/views/layouts/chooseDSS.blade.php
@@ -0,0 +1,62 @@
+<div class="flex flex-col gap-10">
+
+    <h1 class="text-DDS-blue text-3xl font-semibold text-center md:text-4xl xl:text-5xl">Why Choose DSS?</h1>
+
+    <div class=" flex flex-col gap-6 lg:gap-3 xl:gap-2 xl:mt-8">
+        
+            <div class="flex flex-col gap-6 lg:flex-row lg:gap-3 xl:flex-row xl:gap-2">
+
+                <div class="border-4 border-DDS-blue rounded-lg w-[20rem] h-[19rem] sm:w-[28rem] sm:h-[16rem] md:w-[32rem] md:h-[20rem] lg:w-[28rem] lg:h-[18rem] xl:w-[35rem] xl:h-[19rem]">
+                    <div class="pl-10 pt-6 flex flex-row justify-between">
+                        <h1 class="font-semibold text-2xl text-DDS-blue md:text-3xl">Essential Skills</h1>
+                        <img src="{{ asset('pictures/skills_icon.png') }}" class="h-[2rem] mr-10 sm:h-[1.8rem] sm:mt-0.5 md:h-[2rem] md:mt-0 md:mr-16 xl:pr-9 xl:mr-4 xl:h-[2.1rem]">
+                    </div>
+                    <div class="px-10 py-5 md:py-5">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae sapiente quas quaerat dignissimos asperiores suscipit, quo eum enim fugiat, voluptate fugit excepturi mollitia eveniet sequi modi vitae autem facere similique.</p>
+                    </div>
+                </div>
+
+
+                <div class="bg-DDS-blue rounded-lg w-[20rem] h-[19rem] sm:w-[28rem] sm:h-[16rem] md:w-[32rem] md:h-[20rem] lg:w-[28rem] lg:h-[18rem] xl:w-[35rem] xl:h-[19rem]">
+                    <div class="pl-10 pt-6 flex flex-row justify-between">
+                        <h1 class="font-semibold text-2xl text-white md:text-3xl">Language Skills</h1>
+                        <img src="{{ asset('pictures/language_icon.png') }}" class="h-[2rem] mr-10 sm:h-[1.8rem] sm:mt-0.5 md:h-[2rem] md:mt-0 md:mr-16 xl:pr-9 xl:mr-4 xl:h-[2.1rem]"> 
+                    </div>
+                    <div class="px-10 py-5 md:py-5">
+                        <p class="text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, ducimus quasi deleniti qui molestiae quod soluta? Corporis velit dolor earum deserunt eius aliquam molestias quae, harum, non nisi tenetur nemo!</p>
+                    </div>
+                    
+                </div>
+            </div>
+            
+            
+                <div class="flex flex-col gap-6 lg:flex-row lg:gap-3 xl:flex-row xl:gap-2">
+    
+                    <div class=" bg-white border-DDS-blue border-4 rounded-lg w-[20rem] h-[19rem] sm:w-[28rem] sm:h-[16rem] md:w-[32rem] md:h-[20rem] lg:w-[28rem] lg:h-[18rem] lg:bg-DDS-blue xl:w-[35rem] xl:h-[19rem] ">
+                        <div class="pl-10 pt-6 flex flex-row justify-between">
+                            <h1 class="font-semibold text-2xl text-DDS-blue lg:text-white">Academic Fundamentals</h1>
+                            <img src="{{ asset('pictures/fundamentals_icon.png') }}" class="h-[2.5rem] mr-10 mt-2 sm:h-[1.8rem] sm:mt-0.5 md:h-[2rem] md:mt-0 md:mr-16 xl:pr-9 xl:mr-4 xl:h-[2.1rem] hidden lg:block">
+                        </div>
+    
+                        <div class="px-10 py-5 md:py-5">
+                            <p class="text-black lg:text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, ducimus quasi deleniti qui molestiae quod soluta? Corporis velit dolor earum deserunt eius aliquam molestias quae, harum, non nisi tenetur nemo!</p>
+                        </div>
+                        
+                    </div>
+    
+                    
+                    <div class="bg-DDS-blue rounded-lg w-[20rem] h-[19rem] sm:w-[28rem] sm:h-[16rem] md:w-[32rem] md:h-[20rem] lg:w-[28rem] lg:h-[18rem] lg:bg-white lg:border-DDS-blue lg:border-4 xl:w-[35rem] xl:h-[19rem]">
+                        <div class="pl-10 pt-6 flex flex-row justify-between">
+                            <h1 class="font-semibold text-2xl text-white lg:text-DDS-blue">Extra Curricular Activities</h1>
+                            <img src="{{ asset('pictures/eca_icon.png') }}" class="h-[2.5rem] mr-10 mt-2 sm:h-[1.8rem] sm:mt-0.5 md:h-[2rem] md:mt-0 md:mr-16 xl:pr-9 xl:mr-4 xl:h-[2.1rem] hidden lg:block">
+                        </div>
+                        <div class="px-10 py-5 md:py-5">
+                            <p class="text-white lg:text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sequi officiis iusto eum vitae, voluptatum alias repellat facilis sunt placeat quam provident officia harum iste minus maiores praesentium dolore totam.</p>
+                        </div>
+                    </div>
+                    
+                </div>
+
+    </div>
+
+</div>
