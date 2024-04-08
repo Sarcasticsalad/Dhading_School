530d5b9d0ad2c061781fc3ce2a488ca161528367
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-08 11:03:14 +0545

    Updated text size in nav bar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,11 +3,11 @@
          <img src="{{ asset('pictures/DDS_logo.png') }}" class="h-[3rem] mt-6 ml-7 sm:h-[3.5rem] sm:ml-10 md:h-[4rem] md:ml-10 lg:h-[4.5rem] lg:ml-16 lg:mt-8 2xl:h-[4.5rem] 2xl:ml-36">
          <div class="nav-links absolute bg-white w-full mt-7 px-5 top-[7%] pb-8 pt-4 hidden sm:mt-12 sm:px-8 md:mt-16 md:px-10 lg:mt-24 lg:px-16 xl:static xl:block xl:mt-7 2xl:static 2xl:block">
                 <div class="flex flex-col mt-1 gap-8 lg:flex-col lg:gap-8 lg:mr-8 xl:flex-row xl:justify-end xl:mr-0 2xl:mr-19">
-                    <a href="" onclick="onToggleAboutus(this)" class="font-semibold text-gray-800 hover:text-DDS-blue px-3 py-2 lg:text-2xl 2xl:text-3xl ">About Us</a>
-                    <a href="" class="font-semibold  text-gray-800 hover:text-DDS-blue px-3 py-2 lg:text-2xl 2xl:text-3xl">Courses</a>
-                    <a href="" class="font-semibold  text-gray-800 hover:text-DDS-blue px-3 py-2 lg:text-2xl 2xl:text-3xl">Admission</a>
-                    <a href="" class="font-semibold text-gray-800 hover:text-DDS-blue px-3 py-2 lg:text-2xl 2xl:text-3xl">Academics</a>
-                    <a href="" class="font-semibold  text-gray-800 hover:text-DDS-blue px-3 py-2 lg:text-2xl 2xl:text-3xl" >Contact</a>
+                    <a href="" onclick="onToggleAboutus(this)" class="font-semibold text-gray-800 hover:text-DDS-blue px-3 py-2 lg:text-2xl">About Us</a>
+                    <a href="" class="font-semibold  text-gray-800 hover:text-DDS-blue px-3 py-2 lg:text-2xl ">Courses</a>
+                    <a href="" class="font-semibold  text-gray-800 hover:text-DDS-blue px-3 py-2 lg:text-2xl ">Admission</a>
+                    <a href="" class="font-semibold text-gray-800 hover:text-DDS-blue px-3 py-2 lg:text-2xl ">Academics</a>
+                    <a href="" class="font-semibold  text-gray-800 hover:text-DDS-blue px-3 py-2 lg:text-2xl " >Contact</a>
                 </div>
         </div>
         
