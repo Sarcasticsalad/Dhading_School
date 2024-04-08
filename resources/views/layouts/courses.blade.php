a4a5d4d1ff05ab4f4237b33129ba76fdb94eee3c
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-08 14:29:11 +0545

    Created the medium screen view for courses

--- resources/views/layouts/courses.blade.php
+++ resources/views/layouts/courses.blade.php
@@ -1,10 +1,10 @@
-<div class="flex flex-col mt-20">
+<div class="flex flex-col mt-28" id="Courses">
 
     <div class="flex justify-center">
         <h1 class="text-DDS-blue text-3xl font-semibold md:text-4xl lg:5xl 2xl:text-5xl">Our Courses</h1>
     </div>
     
-    <div class="flex flex-col mt-12 gap-14 items-center lg:flex-row lg:justify-evenly px-14">
+    <div class="flex flex-col mt-20 gap-14 items-center md:hidden lg:block lg:flex lg:flex-row lg:justify-evenly px-14">
 
         <div class="cursor-pointer">
             <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
@@ -29,7 +29,7 @@
         <div class="cursor-pointer">
             <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
                 <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
-                    <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-20 xl:bottom-32">
+                    <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-20 xl:bottom-24 2xl:bottom-[7.7rem]">
                         <h1 class="text-DDS-blue font-semibold text-2xl text-center 2xl:text-3xl">Sanskrit</h1>
                     </div>
                 </div>
@@ -39,13 +39,63 @@
         <div class="cursor-pointer">
             <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
                 <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
-                    <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-20 xl:bottom-32">
+                    <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-20 xl:bottom-24 2xl:bottom-[7.7rem]">
                         <h1 class="text-DDS-blue font-semibold text-2xl text-center 2xl:text-3xl">Coding</h1>
                     </div>
                 </div>
             </div>
         </div>
 
+    </div>
+
+    <div class="hidden md:block md:flex md:flex-col lg:hidden xl:hidden 2xl:hidden mt-16 gap-14">
+
+        <div class="flex flex-row gap-16 justify-center">
+
+            <div class="cursor-pointer">
+                <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                        <div class="absolute w-full bottom-24 sm:bottom-28 lg:bottom-16 xl:bottom-24 2xl:bottom-32">
+                            <h1 class="text-DDS-blue font-semibold text-2xl text-center px-2 xl:px-4 2xl:text-3xl">English Learning</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="cursor-pointer">
+                <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                        <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-16 xl:bottom-24 2xl:bottom-32">
+                            <h1 class="text-DDS-blue font-semibold text-2xl text-center px-3 xl:px-4 2xl:text-3xl">Nepali Learning</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-row gap-16 justify-center">
+            <div class="cursor-pointer">
+                <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                        <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-20 xl:bottom-32">
+                            <h1 class="text-DDS-blue font-semibold text-2xl text-center 2xl:text-3xl">Sanskrit</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="cursor-pointer">
+
+                <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                        <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-20 xl:bottom-32">
+                            <h1 class="text-DDS-blue font-semibold text-2xl text-center 2xl:text-3xl">Coding</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
     
     </div>
