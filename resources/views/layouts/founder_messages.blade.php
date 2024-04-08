dc11c25b2fc1fd17175b62a23e9613c2abf4336b
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-08 18:31:28 +0545

    Added the mobile view for Founder's message section

---
+++ resources/views/layouts/founder_messages.blade.php
@@ -0,0 +1,46 @@
+<div class="flex flex-col gap-12 sm:gap-24 lg:hidden ">
+
+    <div class="flex justify-center">
+
+        <div class="flex flex-col gap-5 sm:gap-7">
+            <h1 class="text-center text-DDS-blue text-3xl font-semibold md:text-4xl">Founder's Greetings</h1>
+            <img src="{{ asset('pictures/Shahzada Dawood.jpeg') }}" class="rounded-full border-2 border-DDS-blue w-[10rem] sm:w-[16rem] md:w-[20rem] mx-auto">
+            <p class="px-8 sm:px-16 md:px-24 text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod unde nulla delectus fuga libero ex cumque officia cum architecto soluta voluptatibus tempore eligendi obcaecati aliquid, voluptatum alias quam. Beatae, laboriosam.</p>
+            <p class="font-sans font-semibold text-center md:text-xl">-Rudra Pandey, PhD</p>
+        </div>
+    
+    </div>
+    
+    <div class="flex justify-center">
+    
+        <div class="flex flex-col gap-5 sm:gap-7">
+            <h1 class="text-center text-DDS-blue text-3xl font-semibold md:text-4xl">Principal's Message</h1>
+            <img src="{{ asset('pictures/Shahzada Dawood.jpeg') }}" class="rounded-full border-2 border-DDS-blue w-[10rem] sm:w-[16rem] md:w-[20rem] mx-auto">
+            <p class="px-8 sm:px-16 md:px-24 text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod unde nulla delectus fuga libero ex cumque officia cum architecto soluta voluptatibus tempore eligendi obcaecati aliquid, voluptatum alias quam. Beatae, laboriosam.</p>
+            <p class="font-sans font-semibold text-center md:text-xl">-Bijay Shrestha</p>
+        </div>
+    
+    </div>
+
+    <div class="flex justify-center">
+    
+        <div class="flex flex-col gap-5 sm:gap-7">
+            <h1 class="text-center text-DDS-blue text-3xl font-semibold md:text-4xl">Principal's Message</h1>
+            <img src="{{ asset('pictures/Shahzada Dawood.jpeg') }}" class="rounded-full border-2 border-DDS-blue w-[10rem] sm:w-[16rem] md:w-[20rem] mx-auto">
+            <p class="px-8 sm:px-16 md:px-24 text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod unde nulla delectus fuga libero ex cumque officia cum architecto soluta voluptatibus tempore eligendi obcaecati aliquid, voluptatum alias quam. Beatae, laboriosam.</p>
+            <p class="font-sans font-semibold text-center md:text-xl">-Rudra Pandey, PhD</p>
+        </div>
+    
+    </div>
+
+</div>
+
+
+<div class="flex flex-col hidden lg:block">
+
+    <div>
+        
+
+    </div>
+
+</div>
