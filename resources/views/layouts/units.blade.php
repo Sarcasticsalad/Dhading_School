e6d89114c32b66a0753cabb2173f55f2bc5ab7f8
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-08 14:30:11 +0545

    Updated the units with logo and navigations links to different websites

--- resources/views/layouts/units.blade.php
+++ resources/views/layouts/units.blade.php
@@ -1,16 +1,16 @@
-<div class="flex flex-col mt-28">
+<div class="flex flex-col mt-32 sm:mt-28 xl:mt-32">
 
     <div class="flex justify-center">
         <h1 class="text-DDS-blue text-3xl font-semibold md:text-4xl lg:5xl 2xl:text-5xl">Our Units</h1>
     </div>
     
-    <div class="flex flex-col mt-12 gap-14 items-center lg:flex-row lg:justify-evenly px-14">
+    <div class="flex flex-col mt-20 gap-14 items-center sm:mt-14 xl:mt-20 md:hidden lg:flex lg:flex-row lg:justify-evenly px-14">
 
         <div class="cursor-pointer">
             <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
                 <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
-                    <div class="absolute w-full bottom-24 sm:bottom-28 lg:bottom-16 xl:bottom-24 2xl:bottom-32">
-                        <img src="{{ asset('') }}">
+                    <div class="absolute w-full bottom-[5rem] sm:bottom-[5.8rem] lg:bottom-[3.9rem] xl:bottom-[4.7rem] 2xl:bottom-[7rem]">
+                        <a href="https://deerwalk.edu.np/sifalschool/"><img src="{{ asset('pictures/DSS blue.png') }}" class="w-[11.5rem] ml-5 sm:w-[12rem] sm:ml-8 lg:w-[10rem] lg:mx-4 xl:w-[12rem] xl:mx-4 2xl:w-[15rem] 2xl:mx-9"></a>
                     </div>
                 </div>
             </div>
@@ -19,8 +19,8 @@
         <div class="cursor-pointer">
             <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
                 <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
-                    <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-16 xl:bottom-24 2xl:bottom-32">
-                        <img src="{{ asset('') }}">
+                    <div class="absolute bottom-[5rem] w-full sm:bottom-[5.9rem] lg:bottom-16 xl:bottom-[4.9rem] 2xl:bottom-[6.7rem]">
+                        <a href="https://deerwalk.edu.np/DWIT/"><img src="{{ asset('pictures/DWIT blue.png') }}" class="w-[11.5rem] mx-5 sm:w-[12rem] sm:ml-8 lg:w-[9.6rem] lg:mx-5 xl:w-[11.7rem] xl:mx-4 2xl:w-[15rem] 2xl:mx-9"></a>
                     </div>
                 </div>
             </div>
@@ -29,8 +29,8 @@
         <div class="cursor-pointer">
             <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
                 <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
-                    <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-20 xl:bottom-32">
-                        <img src="{{ asset('') }}">
+                    <div class="absolute bottom-[4.5rem] w-full sm:bottom-[5.5rem] lg:bottom-[3.9rem] xl:bottom-[4.6rem] 2xl:bottom-[6.5rem]">
+                        <a href="https://dlc.dwit.edu.np/"><img src="{{ asset('pictures/DLC blue.png') }}" class="w-[13rem] mx-4 sm:w-[13.9rem] sm:ml-6 lg:w-[11rem] lg:mx-3 xl:w-[13.1rem] xl:mx-4 2xl:w-[17rem] 2xl:mx-9" ></a>
                     </div>
                 </div>
             </div>
@@ -39,13 +39,63 @@
         <div class="cursor-pointer">
             <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
                 <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
-                    <div class="absolute bottom-24 w-full sm:bottom-28 lg:bottom-20 xl:bottom-32">
-                        <img src="{{ asset('') }}">
+                    <div class="absolute bottom-[3.5rem] w-full sm:bottom-[4.3rem] lg:bottom-[3.1rem] xl:bottom-[3.6rem] 2xl:bottom-[5rem]">
+                        <a href="https://deerwalktrainingcenter.com/"><img src="{{ asset('pictures/DTC blue.png') }}" class="sm:w-[14.9rem] sm:ml-4 lg:w-[12rem] lg:mx-1 xl:w-[14rem] 2xl:w-[18.5rem] 2xl:mx-5"></a>
                     </div>
                 </div>
             </div>
         </div>
 
+    </div>
+
+    <div class="hidden md:block md:flex md:flex-col lg:hidden mt-16 gap-14">
+
+        <div class="flex flex-row gap-16 justify-center">
+
+            <div class="cursor-pointer">
+                <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                        <div class="absolute w-full bottom-[5.7rem] ">
+                            <a href=""><img src="{{ asset('pictures/DSS blue.png') }}" class="w-[13rem] mx-5"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="cursor-pointer">
+                <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                        <div class="absolute bottom-[5.8rem] w-full">
+                            <a href=""><img src="{{ asset('pictures/DWIT blue.png') }}" class="w-[12.7rem] mx-7"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-row gap-16 justify-center">
+            <div class="cursor-pointer">
+                <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                        <div class="absolute bottom-[5.3rem] w-full">
+                            <a href=""><img src="{{ asset('pictures/DLC blue.png') }}" class="w-[14.5rem] mx-4"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="cursor-pointer">
+
+                <div class="relative w-[14rem] h-[14rem] bg-DDS-blue rounded-lg sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                    <div class="absolute rounded-lg bottom-2.5 right-2.5 bg-slate-200 w-[14rem] h-[14rem] sm:w-[16rem] sm:h-[16rem] lg:w-[12rem] lg:h-[12rem] xl:w-[14rem] xl:h-[14rem] 2xl:w-[20rem] 2xl:h-[20rem]">
+                        <div class="absolute bottom-[4.2rem]">
+                            <a href=""><img src="{{ asset('pictures/DTC blue.png') }}" class="w-[16rem] mx-1"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
     
     </div>
