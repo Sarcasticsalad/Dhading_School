de3aa9b1affe6c387e582107b527dead84b6e283
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-09 19:28:02 +0545

    Updates made to the size of cards

--- resources/views/layouts/schools.blade.php
+++ resources/views/layouts/schools.blade.php
@@ -1,47 +1,48 @@
-<div id="Schools" class="flex flex-col gap-14 sm:gap-16 lg:gap-20">
+<div id="Schools" class="flex flex-col gap-8 sm:gap-16 lg:gap-20">
   
     <div class="flex justify-center">
-        <h1 class="text-DDS-blue font-semibold text-3xl sm:text-4xl md:text-4xl lg:text-4xl xl:text-5xl">Our Schools</h1>
+        <h1 class="text-DDS-blue font-arial text-3xl sm:text-4xl md:text-4xl lg:text-4xl xl:text-5xl">Our Schools</h1>
     </div>
 
-    <div class="flex flex-col gap-24 sm:gap-28 lg:gap-32 xl:flex-row xl:gap-16 2xl:gap-28">
+    <div class="flex flex-col gap-10 sm:gap-20 lg:gap-28 xl:flex-row xl:gap-16 2xl:gap-28">
+
         <div class="cursor-pointer">
-            <div class="relative h-[19rem] bg-DDS-blue rounded-xl w-[14rem] sm:h-[22rem] sm:w-[20rem] lg:h-[26rem] lg:w-[24rem] xl:w-[21rem] xl:h-[26rem] 2xl:w-[22rem] 2xl:h-[28rem]">
-                <div class="absolute bottom-1.5 right-1.5">
-                    <img src="{{ asset('pictures/Primary School.jpg') }}" class="rounded-t-xl h-[14rem] w-full sm:h-[17rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[22.5rem] object-cover">
-                    <div class="absolute h-[14rem] sm:h-[17rem] lg:h-[20.8rem] 2xl:h-[22.5rem] w-full bg-black/40 flex items-center justify-center bottom-[5.3rem] 2xl:bottom-[5.7rem] rounded-t-xl opacity-0 hover:opacity-100 transition-all duration-300">
-                        <p class="text-white text-2xl font-semibold">Nursery to 3</p>
+            <div class="relative bg-DDS-blue rounded-3xl w-[18rem] h-[21.9rem] sm:h-[26.8rem] sm:w-[20rem] lg:h-[30.5rem] lg:w-[24rem] xl:w-[23rem] xl:h-[30.9rem] 2xl:w-[27rem] 2xl:h-[38rem]">
+                <div class="absolute bottom-3.5 right-3">
+                    <img src="{{ asset('pictures/Primary School.jpg') }}" class="rounded-t-3xl h-[14rem] w-full sm:h-[17.9rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[27.8rem] object-cover">
+                    <div class="absolute h-[14rem] sm:h-[17.9rem] lg:h-[20.6rem] xl:h-[20.5rem] 2xl:h-[27.8rem] w-full bg-black/40 flex items-center justify-center bottom-[7.5rem] sm:bottom-[8.8rem] md:bottom-[8.8rem] lg:bottom-[10rem] xl:bottom-[10.2rem] 2xl:bottom-[10.2rem] rounded-t-3xl opacity-0 hover:opacity-100 transition-all duration-300">
+                        <p class="text-white text-3xl md:text-4xl lg:text-4xl font-arial">Nursery to 3</p>
                     </div>
-                    <div class="bg-gray-200 flex justify-center rounded-b-xl">
-                        <h1 class="py-7 text-lg sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-semibold">Elementary School</h1> 
+                    <div class="bg-gray-200 flex justify-center rounded-b-2xl">
+                        <h1 class="py-12 lg:py-16 sm:py-14 text-base sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-arial">Elementary School</h1> 
                     </div>
                 </div>
             </div>  
         </div>
 
         <div class="cursor-pointer">
-            <div class="relative h-[19rem]  bg-DDS-blue rounded-xl w-[14rem] sm:h-[22rem] sm:w-[20rem] lg:h-[26rem] lg:w-[24rem] xl:w-[21rem] xl:h-[26rem] 2xl:w-[22rem] 2xl:h-[28rem]">
-                <div class="absolute bottom-1.5 right-1.5">
-                    <img src="{{ asset('pictures/Middle School.jpg') }}" class="rounded-t-xl h-[14rem] w-full sm:h-[17rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[22.5rem] object-cover">
-                    <div class="absolute h-[14rem] sm:h-[17rem] lg:h-[20.8rem] 2xl:h-[22.5rem] w-full bg-black/40 flex items-center justify-center bottom-[5.3rem] 2xl:bottom-[5.7rem] rounded-t-xl opacity-0 hover:opacity-100 transition-all duration-300">
-                        <p class="text-white text-2xl font-semibold">Grade 4 to 7</p>
+            <div class="relative bg-DDS-blue rounded-3xl w-[18rem] h-[21.9rem] sm:h-[26.8rem] sm:w-[20rem] lg:h-[30.5rem] lg:w-[24rem] xl:w-[23rem] xl:h-[30.9rem] 2xl:w-[27rem] 2xl:h-[38rem]">
+                <div class="absolute bottom-3.5 right-3">
+                    <img src="{{ asset('pictures/Middle School.jpg') }}" class="rounded-t-3xl h-[14rem] w-full sm:h-[17.9rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[27.8rem] object-cover">
+                    <div class="absolute h-[14rem] sm:h-[17.9rem] lg:h-[20.6rem] xl:h-[20.5rem] 2xl:h-[27.8rem] w-full bg-black/40 flex items-center justify-center bottom-[7.8rem] sm:bottom-[8.5rem] md:bottom-[8.8rem] lg:bottom-[10rem] xl:bottom-[10.2rem] 2xl:bottom-[10.2rem] rounded-t-3xl opacity-0 hover:opacity-100 transition-all duration-300">
+                        <p class="text-white text-3xl md:text-4xl lg:text-4xl font-arial">Grade 4 to 7</p>
                     </div>
-                    <div class="bg-gray-200 flex justify-center rounded-b-xl">
-                        <h1 class="py-7 text-lg sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-semibold">Middle School</h1>
+                    <div class="bg-gray-200 flex justify-center rounded-b-2xl">
+                        <h1 class="py-12 lg:py-16 sm:py-14 text-lg sm:text-base lg:text-2xl xl:text-3xl text-DDS-blue font-arial">Middle School</h1>
                     </div>
                 </div>
             </div> 
         </div>
 
         <div class="cursor-pointer">
-            <div class="relative h-[19rem]  bg-DDS-blue rounded-xl w-[14rem] sm:h-[22rem] sm:w-[20rem] lg:h-[26rem] lg:w-[24rem] xl:w-[21rem] xl:h-[26rem] 2xl:w-[22rem] 2xl:h-[28rem]">
-                <div class="absolute bottom-1.5 right-1.5">
-                    <img src="{{ asset('pictures/High School.jpg') }}" class="rounded-t-xl h-[14rem] w-full sm:h-[17rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[22.5rem] object-cover">
-                    <div class="absolute h-[14rem] sm:h-[17rem] lg:h-[20.8rem] 2xl:h-[22.5rem] w-full bg-black/40 flex items-center justify-center bottom-[5.3rem] 2xl:bottom-[5.7rem] rounded-t-xl opacity-0 hover:opacity-100 transition-all duration-300">
-                        <p class="text-white text-2xl font-semibold">Grade 8 to 10</p>
+            <div class="relative bg-DDS-blue rounded-3xl w-[18rem] h-[21.9rem] sm:h-[26.8rem] sm:w-[20rem] lg:h-[30.5rem] lg:w-[24rem] xl:w-[23rem] xl:h-[30.9rem] 2xl:w-[27rem] 2xl:h-[38rem]">
+                <div class="absolute bottom-3.5 right-3">
+                    <img src="{{ asset('pictures/High School.jpg') }}" class="rounded-t-3xl h-[14rem] w-full sm:h-[17.9rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[27.8rem] object-cover">
+                    <div class="absolute h-[14rem] sm:h-[17.9rem] lg:h-[20.6rem] xl:h-[20.5rem] 2xl:h-[27.8rem] w-full bg-black/40 flex items-center justify-center bottom-[7.5rem] sm:bottom-[8.8rem] md:bottom-[8.8rem] lg:bottom-[10rem] xl:bottom-[10.2rem] 2xl:bottom-[10.2rem] rounded-t-3xl opacity-0 hover:opacity-100 transition-all duration-300">
+                        <p class="text-white text-3xl md:text-4xl lg:text-4xl font-arial">Grade 8 to 10</p>
                     </div>
-                    <div class="bg-gray-200 flex justify-center rounded-b-xl">
-                        <h1 class="py-7 text-lg sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-semibold">High School</h1>
+                    <div class="bg-gray-200 flex justify-center rounded-b-2xl">
+                        <h1 class="py-12 lg:py-16 sm:py-14 text-base sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-arial">High School</h1>
                     </div>
                 </div>
             </div> 
