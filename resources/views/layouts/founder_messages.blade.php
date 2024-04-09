ab9a99027c3f31ac5febd471404f0a275901679c
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-09 09:25:43 +0545

    Updated the founder's message section with responsiveness for larger screens

--- resources/views/layouts/founder_messages.blade.php
+++ resources/views/layouts/founder_messages.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col gap-12 sm:gap-24 lg:hidden ">
+<div class="flex flex-col gap-12 sm:gap-24 lg:hidden xl:hidden 2xl:hidden">
 
     <div class="flex justify-center">
 
@@ -38,9 +38,43 @@
 
 <div class="flex flex-col hidden lg:block">
 
-    <div>
-        
+    <div class="flex flex-row justify-evenly">
 
+        <img src="{{ asset('pictures/Shahzada Dawood.jpeg') }}" class="rounded-full lg:w-[20rem] xl:w-[24rem] 2xl:w-[28rem] border-4 border-DDS-blue">
+
+        <div class="flex flex-col lg:gap-10 w-1/3 mt-10">
+           <h1 class="text-DDS-blue text-3xl font-semibold">Founder's Greetings</h1>
+           <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur, quas suscipit quo impedit placeat beatae deleniti! Dolor magni facilis incidunt, consequuntur ipsum architecto at tempora culpa ratione eveniet cumque adipisci.</p>
+           <p class="text-xl font-semibold">-Rudra Pandey, PhD</p> 
+        </div>
+    
     </div>
 
+    <div class="flex flex-row justify-evenly mt-16">
+
+        <div class="flex flex-col lg:gap-10 w-1/3 mt-10">
+           <h1 class="text-DDS-blue text-3xl font-semibold">Founder's Greetings</h1>
+           <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur, quas suscipit quo impedit placeat beatae deleniti! Dolor magni facilis incidunt, consequuntur ipsum architecto at tempora culpa ratione eveniet cumque adipisci.</p>
+           <p class="text-xl font-semibold">-Rudra Pandey, PhD</p> 
+        </div>
+
+        <img src="{{ asset('pictures/Shahzada Dawood.jpeg') }}" class="rounded-full lg:w-[20rem] xl:w-[24rem] 2xl:w-[28rem] border-4 border-DDS-blue">
+    
+    </div>
+
+
+    <div class="flex flex-row justify-evenly mt-20">
+
+        <img src="{{ asset('pictures/Shahzada Dawood.jpeg') }}" class="rounded-full lg:w-[20rem] xl:w-[24rem] 2xl:w-[28rem] border-4 border-DDS-blue">
+
+        <div class="flex flex-col lg:gap-10 w-1/3 mt-10">
+           <h1 class="text-DDS-blue text-3xl font-semibold">Founder's Greetings</h1>
+           <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur, quas suscipit quo impedit placeat beatae deleniti! Dolor magni facilis incidunt, consequuntur ipsum architecto at tempora culpa ratione eveniet cumque adipisci.</p>
+           <p class="text-xl font-semibold">-Rudra Pandey, PhD</p> 
+        </div>
+    
+    </div>
+   
+
 </div>
+
