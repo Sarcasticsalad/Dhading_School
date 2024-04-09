59a948e8a8d0f74b7e23761c13fd76d631042730
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-09 09:26:32 +0545

    Added the hover effect for school cards

--- resources/views/layouts/schools.blade.php
+++ resources/views/layouts/schools.blade.php
@@ -9,17 +9,23 @@
             <div class="relative h-[19rem] bg-DDS-blue rounded-xl w-[14rem] sm:h-[22rem] sm:w-[20rem] lg:h-[26rem] lg:w-[24rem] xl:w-[21rem] xl:h-[26rem] 2xl:w-[22rem] 2xl:h-[28rem]">
                 <div class="absolute bottom-1.5 right-1.5">
                     <img src="{{ asset('pictures/Primary School.jpg') }}" class="rounded-t-xl h-[14rem] w-full sm:h-[17rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[22.5rem] object-cover">
+                    <div class="absolute h-[14rem] sm:h-[17rem] lg:h-[20.8rem] 2xl:h-[22.5rem] w-full bg-black/40 flex items-center justify-center bottom-[5.3rem] 2xl:bottom-[5.7rem] rounded-t-xl opacity-0 hover:opacity-100 transition-all duration-300">
+                        <p class="text-white text-2xl font-semibold">Nursery to 3</p>
+                    </div>
                     <div class="bg-gray-200 flex justify-center rounded-b-xl">
-                        <h1 class="py-7 text-lg sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-semibold">Elementary School</h1>
+                        <h1 class="py-7 text-lg sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-semibold">Elementary School</h1> 
                     </div>
                 </div>
-            </div> 
+            </div>  
         </div>
 
         <div class="cursor-pointer">
             <div class="relative h-[19rem]  bg-DDS-blue rounded-xl w-[14rem] sm:h-[22rem] sm:w-[20rem] lg:h-[26rem] lg:w-[24rem] xl:w-[21rem] xl:h-[26rem] 2xl:w-[22rem] 2xl:h-[28rem]">
                 <div class="absolute bottom-1.5 right-1.5">
                     <img src="{{ asset('pictures/Middle School.jpg') }}" class="rounded-t-xl h-[14rem] w-full sm:h-[17rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[22.5rem] object-cover">
+                    <div class="absolute h-[14rem] sm:h-[17rem] lg:h-[20.8rem] 2xl:h-[22.5rem] w-full bg-black/40 flex items-center justify-center bottom-[5.3rem] 2xl:bottom-[5.7rem] rounded-t-xl opacity-0 hover:opacity-100 transition-all duration-300">
+                        <p class="text-white text-2xl font-semibold">Grade 4 to 7</p>
+                    </div>
                     <div class="bg-gray-200 flex justify-center rounded-b-xl">
                         <h1 class="py-7 text-lg sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-semibold">Middle School</h1>
                     </div>
@@ -31,6 +37,9 @@
             <div class="relative h-[19rem]  bg-DDS-blue rounded-xl w-[14rem] sm:h-[22rem] sm:w-[20rem] lg:h-[26rem] lg:w-[24rem] xl:w-[21rem] xl:h-[26rem] 2xl:w-[22rem] 2xl:h-[28rem]">
                 <div class="absolute bottom-1.5 right-1.5">
                     <img src="{{ asset('pictures/High School.jpg') }}" class="rounded-t-xl h-[14rem] w-full sm:h-[17rem] lg:h-[20.5rem] xl:h-[20.5rem] 2xl:h-[22.5rem] object-cover">
+                    <div class="absolute h-[14rem] sm:h-[17rem] lg:h-[20.8rem] 2xl:h-[22.5rem] w-full bg-black/40 flex items-center justify-center bottom-[5.3rem] 2xl:bottom-[5.7rem] rounded-t-xl opacity-0 hover:opacity-100 transition-all duration-300">
+                        <p class="text-white text-2xl font-semibold">Grade 8 to 10</p>
+                    </div>
                     <div class="bg-gray-200 flex justify-center rounded-b-xl">
                         <h1 class="py-7 text-lg sm:text-xl lg:text-2xl xl:text-3xl text-DDS-blue font-semibold">High School</h1>
                     </div>
