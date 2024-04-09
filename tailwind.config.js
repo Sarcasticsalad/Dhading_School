b3cb79185757427825b08cf4a7e9e3eaf4ae630c
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-09 19:26:54 +0545

    File added for tailwind build

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,17 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./public/assets/fonts/Arial_Rounded_MT/*.{ttf,woff,woff2}",
   ],
   theme: {
+    fontFamily:{
+      arial : ['Arial Rounded MT'],
+
+    fontWeight:{
+      bold:'bold',
+      }
+    },
+    
     extend: {
       colors: {
         'DDS-blue' : '#00B3E6', 
