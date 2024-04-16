51cf68d6e0400681a9c887f6c140e416135b9153
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-16 15:49:20 +0545

    push

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Deerwalk Dhading School</title>
-    <link rel="icon"  href="/public/android-chrome-192x192.png">
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link href="/resources/css/fonts.css">
     @vite('resources/css/app.css')
