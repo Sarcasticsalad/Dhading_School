9f4f1f326821faa17e3cab6bf03ebd17a0409fd8
Samrajya Basnyat <samrajyabasnyat64@gmail.com>
2024-04-16 10:18:32 +0545

    About us dropdown now closes when clicked outside of the dropdown

--- resources/js/about_us.js
+++ resources/js/about_us.js
@@ -2,23 +2,16 @@ const aboutUsLink = document.getElementById('aboutUsLink');
 const aboutUsDropdown = document.getElementById('aboutUsDropdown');
 
 
+
+
 aboutUsLink.addEventListener('click', function(event){
     event.preventDefault();
     aboutUsDropdown.classList.toggle('hidden');
+
 });
 
-// document.addEventListener('click', function(event){
-    
-//     if(aboutUsDropdown.classList.contains('hidden')) return;
-
-//     if(!aboutUsDropdown.classList.contains('hidden')){
-
-//         if(!aboutUsDropdown.classList.contains(event.target)){
-
-//             aboutUsDropdown.classList.add('hidden');
-//         }
-
-//     }
-
-    
-// });
+document.addEventListener('click', function(event){
+    if(event.target !== aboutUsLink && !aboutUsDropdown.contains(event.target)) {
+        aboutUsDropdown.classList.add('hidden');
+    }
+});
